4c3df4f2db60828562312b14310b4d7c85912331
Miguel Castro <miguel.castro@neofinancial.com>
2023-10-31 20:17:48 -0500

    Improve header styles and layout

--- app/src/main/java/com/example/apiproject/Header.kt
+++ app/src/main/java/com/example/apiproject/Header.kt
@@ -1,39 +1,51 @@
 package com.example.apiproject
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 
 @Composable
 fun AppHeader(navController: NavController) {
-    Row(modifier = Modifier.padding(16.dp)) {
-        Button(onClick = { navController.navigate(NavigationDestinations.SEARCH_SCREEN) }) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        TextButton(
+            onClick = { navController.navigate(NavigationDestinations.SEARCH_SCREEN) },
+            modifier = Modifier.weight(1f)
+        ) {
             Text(text = "Home")
         }
 
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Button(onClick = { navController.navigate(NavigationDestinations.FAVORITES_SCREEN) }) {
+        TextButton(
+            onClick = { navController.navigate(NavigationDestinations.FAVORITES_SCREEN) },
+            modifier = Modifier.weight(1f)
+        ) {
             Text(text = "Favorites")
         }
 
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Button(onClick = { navController.navigate(NavigationDestinations.ABOUT_SCREEN) }) {
+        TextButton(
+            onClick = { navController.navigate(NavigationDestinations.ABOUT_SCREEN) },
+            modifier = Modifier.weight(1f)
+        ) {
             Text(text = "About")
         }
 
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Button(onClick = { navController.navigate(NavigationDestinations.ACCOUNT_SCREEN) }) {
+        TextButton(
+            onClick = { navController.navigate(NavigationDestinations.ACCOUNT_SCREEN) },
+            modifier = Modifier.weight(1f)
+        ) {
             Text(text = "Account")
         }
     }
 }
+
