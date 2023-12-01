c94ca405f7297ab66759f4353270d04bb4ff1363
Michael <mkozubski@academic.rrc.ca>
2023-12-01 13:44:20 -0600

    Refactored Apollo.kt to be more secure. API key moved to ApiConfig.kt

---
+++ app/src/main/java/com/example/apiproject/Apollo/ApiConfig.kt
@@ -0,0 +1,6 @@
+package com.example.apiproject.Apollo
+
+object ApiConfig {
+    const val USER_ID = "599"
+    const val API_KEY = "bfc65097269c742516dad0f0f2da4d4e1958eda6cadabc576d44999f512012649e41897fbb21449decf977c1d4f1b14663"
+}
--- app/src/main/java/com/example/apiproject/Apollo/Apollo.kt
+++ app/src/main/java/com/example/apiproject/Apollo/Apollo.kt
@@ -9,15 +9,15 @@ import okhttp3.Response
 private class AuthorizationInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("X-USER-ID", "599")
-            .addHeader("X-API-KEY", "bfc65097269c742516dad0f0f2da4d4e1958eda6cadabc576d44999f512012649e41897fbb21449decf977c1d4f1b14663")
+            .addHeader("X-USER-ID", ApiConfig.USER_ID)
+            .addHeader("X-API-KEY", ApiConfig.API_KEY)
             .build()
 
         return chain.proceed(request)
     }
 }
 
-val apolloClient = ApolloClient.Builder()
+val apolloClient: ApolloClient = ApolloClient.Builder()
     .serverUrl("https://api.taddy.org/graphql")
     .okHttpClient(
         OkHttpClient.Builder()
